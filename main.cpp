cf4d20dd7669aea78cbdc0811ddad670a2b07bc6
devlongen <iago.mendonca_2005@hotmail.com>
2025-07-19 12:36:07 -0300

    estudando: entendendo alguns passos da linguagem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello World!";
+int soma(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int numero1, numero2;
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> numero1;
+
+    std::cout << "Digite o segundo número: ";
+    std::cin >> numero2;
+
+    int resultado = soma(numero1, numero2);
+
+    std::cout << "A soma é: " << resultado << std::endl;
+
     return 0;
 }
