0d1992d5912423c3ed34c725d1af6a57b61ddc42
devlongen <iago.mendonca_2005@hotmail.com>
2025-07-20 14:53:54 -0300

    func: criado a função do calculo aerodinamico, fazendo testes de forma bem simples mas progredindo :)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
-float formula_aerodinamica(){
+float cx_car(std::string usuario){
+    float coeficiente_aerodinamico;
     float forca_de_arrasto;
     float p_densidade;
-    float cx_aerodinamico;
-    float a_frontal;
-    float velocidade;
+    float a_referencia_frontal;
+    float velocidade_objeto;
 
-    std::cout << "Preenche formula do aerodinamico para aplicar teste de túnel";
+    std::cout << "Olá, seja bem vindo caro: " << usuario << std::endl << "Que aproveite esse programa!";
+    std::cout << "Vamos trabalhar com a formula para dizer força de arrasto ou coeficiente aerodinamico de um carro..." << std::endl;
+    std::cout << "Intuito deste cálculo para depois aplicar no teste de túnel para entendermos a performance em rua ou simular." << std::endl;
+    std::cout << "Começaremos te perguntando, sobre os valores perante ao carro" << std::endl;
+
+    std::cout << "Qual seria o coeficiente aerodinamico do carro? (Caso não souber, digite -1)" << std::endl;
+    std::cin >> coeficiente_aerodinamico;
+    std::cout << "Qual número é a força de arrasto? (Newtons)" << std::endl;
     std::cin >> forca_de_arrasto;
+    std::cout << "Qual é a densidade do ar? (kg/m³)" << std::endl;
     std::cin >> p_densidade;
-    std::cin >> cx_aerodinamico;
-    std::cin >> a_frontal;
-    std::cin >> velocidade;
+    std::cout << "Qual é a área de referência do veiculo? (m²)" << std::endl;
+    std::cin >> a_referencia_frontal;
+    std::cout << "Qual velocidade atinge este carro? (m/s)" << std::endl;
+    std::cin >> velocidade_objeto;
+
+    if(coeficiente_aerodinamico != -1){
+    // forca de arrasto
+    forca_de_arrasto = 0.5f * p_densidade * std::pow(velocidade_objeto,2) * a_referencia_frontal * coeficiente_aerodinamico;
+    return forca_de_arrasto;
+}
+    else{
+        // Calculo de coeficiente aerodinamico
+        coeficiente_aerodinamico = (2 * forca_de_arrasto) / (p_densidade * std::pow(velocidade_objeto,2) * a_referencia_frontal);
+        return coeficiente_aerodinamico;
+    }
+    return 0;
 }
 
 int main() {
-    std::cout << "Começando aos trabalhos! heheh";
+    std::string usuario;
+    float resultado;
+    std::cout << "Começando aos trabalhos! heheh" << std::endl;
+    std::cout << "Qual é o seu nome?" << std::endl;
+    std::cout << "--> ";
+    std::cin >> usuario;
+    resultado = cx_car(usuario);
+    std::cout << "Seu resultado é:" << resultado << std::endl;
+    std::cout << "Obrigado por usar o programa!";
     return 0;
 }
